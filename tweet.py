5be3af1bc10224770862a09089f3af7cf4fb1cbb
JacksonMHoke <jacksonmhoke@gmail.com>
2021-01-18 14:48:05 -0800

    Adapted Algorithm in tweet.py to not repeat correctly
    * Changed tweet.py bug where tweets repeat. Caused by not filling last_3
      before entering while loop
    * Changed default value of translations in translator.py
    * Added files to gitignore

--- tweet.py
+++ tweet.py
@@ -3,34 +3,36 @@ from translator import bulk_trans
 from googletrans import Translator
 from collections import deque
 import time
+import json
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("kfT1kMqDb1OYsKAVMByMlIG0I", 
-    "OPwrPcdfC5ipemzO945R76bUStUi5DycQattE7vqHTiOxpfNX9")
-auth.set_access_token("1189949666999226368-nKTwhWRf6ZVx2twnBhRS6lGV1gg0ae", 
-    "M22hdGdUxk6QE21oFvx2zxR4t745ZGUCJrmO2My18YVgV")
+auth = tweepy.OAuthHandler("FExyXveI1drI9wjsj8QIb6sF2", 
+    "Z80bZofhHuW0jBT2YPFiqchHXMSPK52jRH8VH5PaMCC79FWTRN")
+auth.set_access_token("1189949666999226368-cMAeMG4TA3C7CJOk1NLyrWTMNQ9MGA", 
+    "oeKmEte19XxuLylWkypZGVGuY1aljt1P7WPYX5tjT1rZM")
 
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+#create a queue that stores the latest 3 tweets
+last_3 = deque()
+timeline = api.user_timeline("HapaGucci", count=3)
+for tweet in timeline:
+    last_3.appendleft(tweet)
 
 while True:
-    timeline = api.user_timeline("HapaGucci", count=5)
-    last_5 = deque()
+    timeline = api.user_timeline("HapaGucci", count=3)
     for tweet in timeline:
-        if tweet.text not in last_5:
+        if tweet not in last_3:
             #if not found put it into list of found tweets
-            last_5.append(tweet.text)
+            last_3.append(tweet)
             #translate and tweet
             curr = bulk_trans(tweet.text)
             try:
-                api.update_status(curr + '\n\n' + tweet.text)
+                api.update_status(curr)
             except:
                 print('couldn\'t tweet' + curr)
-            #if more than 5 reduce queue by 1
-            last_5.popleft()
+            print(tweet.text)
+    #make sure queue only has 5 elements or less since we tracking last 5 tweets
+    while len(last_3)>3:
+        last_3.popleft()
     time.sleep(60)
